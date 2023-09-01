4e9e70773f59bf8919ee18c869ff8af5748ac3a4
Yapcc00 <121201204+Yapcc00@users.noreply.github.com>
2023-09-01 14:45:46 -0500

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,25 +1,124 @@
---INSERTAR PRODUCTOS 
--- insert into values 
+use Tienda
 
-
-insert into Producto
-(codigo, nombre, variedad, marca, cantidad, precios
-)
+-- Producto 1
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
 values(
 'COD1','LECHE','ENERA','COLANTA',6,1.24
 )
 
-
-insert into Producto
-(codigo, nombre, variedad, marca, cantidad, precios
-)
+-- Producto 2
+INSERT INTO [dbo].[Producto]
+           ([Codigo]
+           ,[Nombre]
+           ,[Variedad]
+           ,[Marca]
+           ,[Cantidad]
+           ,[Precios])
 values(
 'COD2','PAN','TRIGO','DUNO',4,4.24
 )
 
+-- Producto 3
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
+     VALUES
+           ('Cod3'
+           ,'Tomate'
+           ,'natural'
+           ,'Ecohuerto'
+           ,6
+		,1.70)
 
---Selecionar todas las tablas de producto
-select * from Producto 
+-- Producto 4
+INSERT INTO [dbo].[Producto]
+           ([Codigo]
+           ,[Nombre]
+           ,[Variedad]
+           ,[Marca]
+           ,[Cantidad]
+           ,[Precio])
+     VALUES
+           ('Cod4'
+           ,'Leche'
+           ,'Semisdesnatada'
+           ,'Ecovaca'
+           ,6
+		,1.05)
 
---Selecionar el nombre del la tabla producto
-select nombre from Producto 
+-- Producto 5
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
+     VALUES
+           ('Cod5'
+           ,'Leche'
+           ,'Desnatada'
+           ,'Ecovaca'
+           ,6
+		,0.90)
+
+-- Producto 6
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
+     VALUES
+           ('Cod6'
+           ,'Aceite'
+           ,'Freir'
+           ,'Lafritura'
+           ,1
+		,1.79)
+
+-- Producto 7
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
+     VALUES
+           ('Cod7'
+           ,'Cerveza'
+           ,'rubia'
+           ,'LaArtesana'
+           ,1
+		,1.40)
+
+-- Producto 8
+INSERT INTO [dbo].[Producto]
+           ([codigo]
+           ,[nombre]
+           ,[variedad]
+           ,[marca]
+           ,[cantidad]
+           ,[precios])
+     VALUES
+           ('Cod8'
+           ,'Cerveza'
+           ,'Extra'
+           ,'LaArtesana'
+           ,4
+		,2.30)
+
+
+		SELECT * FROM Producto
